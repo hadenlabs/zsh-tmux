730cdfa0d7c0399d2e226844d566cd5dc5989b14
@slovacus <slovacus@gmail.com>
2019-11-23 16:03:01 -0500

    chore: implement functions for install tmux (#3)

---
+++ zsh-tmux.zsh
@@ -0,0 +1,52 @@
+#!/usr/bin/env ksh
+# -*- coding: utf-8 -*-
+
+#
+# Defines install tmux for osx or linux.
+#
+# Authors:
+#   Luis Mayta <slovacus@gmail.com>
+#
+tmux_package_name=tmux
+
+plugin_dir=$(dirname "${0}":A)
+
+# shellcheck source=/dev/null
+source "${plugin_dir}"/src/helpers/messages.zsh
+
+# shellcheck source=/dev/null
+source "${plugin_dir}"/src/helpers/tools.zsh
+
+function tmux::install {
+    message_info "Installing ${tmux_package_name}"
+    if ! type -p brew > /dev/null; then
+        message_error "it's neccesary brew, add: luismayta/zsh-brew"
+    fi
+    brew install tmux
+    message_success "Installed ${tmux_package_name}"
+}
+
+function tpm::install {
+    message_info "Installing tpm for ${tmux_package_name}"
+    git clone https://github.com/tmux-plugins/tpm "${HOME}"/.tmux/plugins/tpm
+    message_success "Installed tpm for ${tmux_package_name}"
+}
+
+if ! type -p tmux > /dev/null; then tmux::install; fi
+if [ ! -e "${HOME}/.tmux/plugins/tpm" ]; then tpm::install; fi
+
+function tmux::dependences {
+    message_info "Installing dependences for ${tmux_package_name}"
+    message_success "Installed dependences for ${tmux_package_name}"
+}
+
+function tmux::post_install {
+    message_info "Post Install ${tmux_package_name}"
+    message_success "Success Install ${tmux_package_name}"
+}
+
+function tmux::sync {
+
+}
+
+tmux::sync
