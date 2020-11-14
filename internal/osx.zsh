940f68cddc2aecb6c1d3f75da93cda51fa8eed52
Luis Mayta <luis@hadenlabs.com>
2020-11-13 22:38:59 -0500

    chore: implement package reattach user namespace (#36)

--- internal/osx.zsh
+++ internal/osx.zsh
@@ -1,2 +1,12 @@
 #!/usr/bin/env ksh
 # -*- coding: utf-8 -*-
+
+function tmux::internal::reattach-to-user-namespace::install {
+    if ! type -p brew > /dev/null; then
+        message_warning "${TMUX_MESSAGE_BREW}"
+        return
+    fi
+    message_info "Installing reattach-to-user-namespace for ${TMUX_PACKAGE_NAME}"
+    brew install reattach-to-user-namespace
+    message_success "Installed reattach-to-user-namespace ${TMUX_PACKAGE_NAME}"
+}
