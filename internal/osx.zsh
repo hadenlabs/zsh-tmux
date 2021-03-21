a38a4b499f11c392bc26aa7a6ac98969e13f9d5e
@slovacus <slovacus@gmail.com>
2021-03-21 12:38:40 -0500

    refactor: validation app with zsh-core (#44)

--- internal/osx.zsh
+++ internal/osx.zsh
@@ -1,14 +1,4 @@
 #!/usr/bin/env ksh
 # -*- coding: utf-8 -*-
 
-function tmux::internal::reattach-to-user-namespace::install {
-    if ! type -p brew > /dev/null; then
-        message_warning "${TMUX_MESSAGE_BREW}"
-        return
-    fi
-    message_info "Installing reattach-to-user-namespace for ${TMUX_PACKAGE_NAME}"
-    brew install reattach-to-user-namespace
-    message_success "Installed reattach-to-user-namespace ${TMUX_PACKAGE_NAME}"
-}
-
-if ! type -p reattach-to-user-namespace > /dev/null; then tmux::internal::reattach-to-user-namespace::install; fi
+if ! core::exists reattach-to-user-namespace; then core::install reattach-to-user-namespace; fi
